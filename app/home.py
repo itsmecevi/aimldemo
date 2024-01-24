232bebf3b42a25f747bfd25f0d050d7d2832a2f6
Cevi Herdian Insight <itsmecevi@gmail.com>
2024-01-24 13:43:54 +0700

    Update home.py

--- app/home.py
+++ app/home.py
@@ -4,7 +4,8 @@ def app():
     st.write("# AI ML Demo")
     st.markdown(
         """
-       This is is a demo hold by itsmecevi.github.io to show the power of AI and ML.**
+       This is is a demo hold by itsmecevi.github.io to show the power of AI and ML. 
+       This demo systems that have been created can be viewed on the sidebar 👈.
         
     
         
