8bb9f397d99888f7065cf2438e6709ef8278d4f1
Cevi Herdian Insight <itsmecevi@gmail.com>
2024-01-24 13:30:06 +0700

    Update home.py

--- app/home.py
+++ app/home.py
@@ -1,21 +1,12 @@
 import streamlit as st
 
 def app():
-    st.write("# about me")
+    st.write("# AI ML Demo")
     st.markdown(
         """
-        I am a computer science lecturer at Universitas Bunda Mulia (UBM) with 5+ years of
-        experience in teaching and research. The research topics I cover include Optimization,
-        Image Processing, Expert Systems, Machine Learning, and Artificial Intelligence.
-        The conclusions of all of my research are published in internationally accredited
-        journals, national journals, or conferences and hope to be feedback for review,
-        replicated, or applied in an organization. Some demo systems that have been created
-        can be **viewed on the sidebar 👈**
+       This is is a demo hold by itsmecevi.github.io to show the power of AI and ML.**
         
-        ### Whare you can find me?
-        - in my publication [Google scholar](https://scholar.google.com/citations?user=LxbIO5sAAAAJ)
-        - in my code documentation [Github](https://github.com/firstime09)
-        - in my social media [LinkedIn](https://id.linkedin.com/in/felliks-feiters-tampinongkol-100084174)
+    
         
-        ### Under Maintenance...    
+            
         """)
